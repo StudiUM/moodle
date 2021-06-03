477eaa062ea65bae8239179ab7460e9f30d34912
Issam Taboubi <issam.taboubi@umontreal.ca>
2021-06-03 14:41:00 -0400

    MDL-71301 core: GapSelect/Match questions - Make labels more meaningful

--- question/type/gapselect/lang/en/qtype_gapselect.php
+++ question/type/gapselect/lang/en/qtype_gapselect.php
@@ -24,6 +24,7 @@
 
 $string['addmorechoiceblanks'] = 'Blanks for {no} more choices';
 $string['answer'] = 'Answer';
+$string['answernumber'] = 'Answer {$a}';
 $string['choices'] = 'Choices';
 $string['choicex'] = 'Choice [[{no}]]';
 $string['combinedcontrolnamegapselect'] = 'drop-down menu';
